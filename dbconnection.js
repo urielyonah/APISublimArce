a64227f57d9a97ad282aad966fd9c65be5f3c5ee
urielyonah~ <urielyonah@gmail.com>
2023-11-03 19:51:55 -0600

    modified:   dbconnection.js 	modified:   server.js

--- dbconnection.js
+++ dbconnection.js
@@ -8,7 +8,13 @@ class DataBase {
             password: 'OCs10YTeZ47cnQ1Rwddp',
             database: 'bf9oexmvjv7umifwuwdu',
             charset: 'utf8',
-        };
+        }
+        /*this.config = {
+            host: 'localhost',
+            user: 'uriel',
+            password: '123456789',
+            database: 'sublimarce01',
+        };*/
         this.connection = mysql.createConnection(this.config);
     }
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 // Configuración de bodyParser para manejar solicitudes JSON
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Reemplaza '*' con el dominio de tu aplicación en producción
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
