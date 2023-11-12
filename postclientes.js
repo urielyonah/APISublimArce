69dc9e2ab479590cca1384f527b613553d36d38d
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-12 16:54:59 -0600

    Probando

--- postclientes.js
+++ postclientes.js
@@ -4,7 +4,7 @@ const DataBase = require('./dbconnection');
 
 const db = new DataBase();
 
-router.post('/register', (req, res) => {
+router.post('/', (req, res) => {
     const con = db.dbconnection();
     const correo = req.body.email;
     const nombre = req.body.name;
@@ -12,30 +12,14 @@ router.post('/register', (req, res) => {
     const telefono = req.body.telefono;
     const direccion = req.body.direccion
 
-    // Consulta para verificar si el correo ya existe
-    const sql = `SELECT COUNT(*) AS total FROM CLIENTES WHERE CORREO = '${correo}'`;
+    const sql = `INSERT INTO CLIENTES (NOMBRE, CORREO, CONTRASEÑA, TELEFONO, DIRECCION) VALUES ('${nombre}', '${correo}', '${contraseña}', '${telefono}', '${direccion}')`;
     con.query(sql, (err, results) => {
         if (err) {
            throw err;
         } else {
-            // Si el correo ya existe, no se registra al usuario
-            if (results.rows[0].total > 0) {
-                res.status(409).json({
-                    message: 'El correo ya existe',
-                });
-            } else {
-                // Si el correo no existe, se procede a registrar al usuario
-                const sql = `INSERT INTO CLIENTES (NOMBRE, CORREO, CONTRASEÑA, TELEFONO, DIRECCION) VALUES ('${nombre}', '${correo}', '${contraseña}', '${telefono}', '${direccion}')`;
-                con.query(sql, (err, results) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        res.status(200).json(results);
-                        console.log('Numero de registros insertados: ' + results.affectedRows);
-                        console.log(nombre, correo, contraseña);
-                    }
-                });
-            }
+            res.status(200).json(results);
+            console.log('Numero de registros insertados: ' + results.affectedRows);
+            console.log(nombre, correo, contraseña);
         }
     });
 });
