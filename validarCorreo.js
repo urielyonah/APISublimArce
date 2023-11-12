e0e88e340469f113f9fd1b81d308f9c0b990ce0f
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-12 16:01:14 -0600

    validarCorreo

--- validarCorreo.js
+++ validarCorreo.js
@@ -4,21 +4,21 @@ const DataBase = require('./dbconnection');
 
 const db = new DataBase();
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     const email = req.body.email;
     if (typeof email === 'string') {
-        const result = await db.request()
-            .input('email', email)
-            .query('SELECT * FROM CLIENTES WHERE CORREO = @email');
-
-        if (result.recordset.length > 0) {
-            res.json(result.recordset);
-        } else {
-            res.json({ success: false });
-        }
-    }else{
-       console.log('La cadena no es valida')
+      const result = db.request()
+        .input('email', email)
+        .query('SELECT * FROM CLIENTES WHERE CORREO = @email');
+  
+      if (result.length > 0) {
+        res.json(result);
+      } else {
+        res.json({ success: false });
+      }
+    } else {
+      console.log('La cadena no es valida');
     }
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
