3d97928362f4b3babc10295ea101a9d075feb739
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-12 15:12:11 -0600

    validarCorreo

---
+++ validarCorreo.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const DataBase = require('./dbconnection');
+
+const db = new DataBase();
+
+router.get('/validarCorreo', async (req, res) => {
+    const correo = req.query.email;
+
+    if (!email) {
+        res.status(400).json({ message: 'Missing email parameter' });
+        return;
+    }
+    try {
+        const result = await db.query('SELECT * FROM CLIENTES WHERE CORREO = ?', [correo]);
+
+        if (result.length > 0) {
+            res.status(200).json({ existeCorreo: true });
+        } else {
+            res.status(200).json({ existeCorreo: false });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error en la consulta' });
+    }
+});
+
+module.exports = router;
