c98745368c75afdbfa22bd735be25020573ac90a
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-21 16:44:54 -0600

    Agregando servicios

--- postServicio.js
+++ postServicio.js
@@ -16,10 +16,10 @@ router.post('/', async (req, res) => {
         const precio = req.body.precio;
     
         // Insertar en la tabla SERVICIOS
-        const servicioId = insertarServicio(con, servicio, tamano, calidad, area, precio, imagen);
+        const servicioId = await insertarServicio(con, servicio, tamano, calidad, area, precio, imagen);
     
         // Insertar en la tabla CAMISAS-SERVICIOS
-        insertarCamisasServicios(con, idCamisa, servicioId, precio);
+        await insertarCamisasServicios(con, idCamisa, servicioId, precio);
     
         res.status(200).json({ message: 'Agregado a pedidos con éxito' });
       } catch (error) {
@@ -30,28 +30,30 @@ router.post('/', async (req, res) => {
     
     // Función para insertar en la tabla SERVICIOS y obtener el ID del servicio insertado
     function insertarServicio(con, tipo, tamano, calidad, area, precio, imagen) {
-        const sql = `INSERT INTO SERVICIOS (TIPO-SERVICIO, tamaño, calidad, AREA, PRECIO, IMAGEN) VALUES (?, ?, ?, ?, ?, ?)`;
-        con.query(sql, [tipo, tamano, calidad, area, precio, imagen], (err, results) => {
-          if (err) {
-            throw err;
-          } else {
-            res.status(200).json(results.insertId);
-          }
+        return new Promise((resolve, reject) => {
+          const sql = `INSERT INTO SERVICIOS (TIPO-SERVICIO, tamaño, calidad, AREA, PRECIO, IMAGEN) VALUES (?, ?, ?, ?, ?, ?)`;
+          con.query(sql, [tipo, tamano, calidad, area, precio, imagen], (err, results) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results.insertId);
+            }
+          });
         });
     }
     
     // Función para insertar en la tabla CAMISAS_SERVICIOS
     function insertarCamisasServicios(con, idCamisa, idServicio, precio) {
-        const sql = `INSERT INTO CAMISAS-SERVICIOS (ID-CAMISAS, ID-SERVICIOS, PRECIO) VALUES (?, ?, ?)`;
-        con.query(sql, [idCamisa, idServicio, precio], (err, result) => {
-          if (err) {
-            throw err;
-          } else {
-            res.status(200).json(result);
-          }
+        return new Promise((resolve, reject) => {
+          const sql = `INSERT INTO CAMISAS-SERVICIOS (ID-CAMISAS, ID-SERVICIOS, PRECIO) VALUES (?, ?, ?)`;
+          con.query(sql, [idCamisa, idServicio, precio], (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          });
         });
-  
-}
+    }
 
 module.exports = router;
-
