573c7801d3638e2ae0cf24714d6e071fa019a609
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-21 21:04:43 -0600

    Agregando servicios

--- postServicio.js
+++ postServicio.js
@@ -6,7 +6,7 @@ const db = new DataBase();
 
 // Función para insertar en la tabla SERVICIOS
 function insertarServicio(con, tipo, tamano, calidad, area, precio, imagen) {
-    const sql = `INSERT INTO SERVICIOS (TIPO_SERVICIO, tamaño, calidad, AREA, PRECIO, IMAGEN) VALUES (?, ?, ?, ?, ?, ?)`;
+    const sql = `INSERT INTO SERVICIOS (TIPO-SERVICIO, tamaño, calidad, AREA, PRECIO, IMAGEN) VALUES (?, ?, ?, ?, ?, ?)`;
     con.query(sql, [tipo, tamano, calidad, area, precio, imagen], (err, results) => {
         if (err) {
             console.error('Error al insertar servicio:', err);
