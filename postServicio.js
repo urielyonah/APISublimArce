c1fb463e10fa8d77571f49117450e0b9ec831bdc
jose maria verdin becerra <verdinbecerraj@gmail.com>
2023-11-21 20:04:54 -0600

    Agregando servicios

--- postServicio.js
+++ postServicio.js
@@ -5,21 +5,20 @@ const db = new DataBase();
 
 // Función para insertar en la tabla SERVICIOS
 function insertarServicio(con, tipo, tamano, calidad, area, precio, imagen) {
-    return new Promise((resolve, reject) => {
         const sql = `INSERT INTO SERVICIOS (TIPO-SERVICIO, tamaño, calidad, AREA, PRECIO, IMAGEN) VALUES (?, ?, ?, ?, ?, ?)`;
         con.query(sql, [tipo, tamano, calidad, area, precio, imagen], (err, results) => {
             if (err) {
                 console.error('Error al insertar servicio:', err);
-                reject({ error: 'Error interno del servidor al insertar servicio' });
+                throw err;
             } else {
-                resolve(results);
+                res.status(200).json(results);
+                console.log(tamano, servicio, calidad, area, precio);
             }
         });
-    });
 }
 
 // Luego en tu ruta POST
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
     try {
         const con = db.dbconnection();
 
@@ -32,9 +31,7 @@ router.post('/', async (req, res) => {
         const precio = req.body.precio;
 
         // Insertar en la tabla SERVICIOS
-        await insertarServicio(con, servicio, tamano, calidad, area, precio, imagen);
-
-
+       insertarServicio(con, servicio, tamano, calidad, area, precio, imagen);
         res.status(200).json({ message: 'Agregado a pedidos con éxito' });
     } catch (error) {
         console.error('Error al agregar a pedidos:', error);
