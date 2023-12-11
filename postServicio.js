c4571173b008fc92141e20c44037a411f0aad928
urielyonah~ <urielyonah@gmail.com>
2023-12-10 19:22:56 -0600

    POSTSERVICIO.JS SIN PROMESAS

--- postServicio.js
+++ postServicio.js
@@ -19,55 +19,52 @@ router.post('/', async (req, res) => {
         const idServicioInsertado = await insertarServicio(con, res, idCamisa, servicio, tamano, calidad, area, precio, imagen);
 
         // Insertar en la tabla CAMISAS-SERVICIOS
-        await insertarCamisasServicios(con, idCamisa, idServicioInsertado, precio);
+        await insertarCamisasServicios(con, res, idCamisa, idServicioInsertado, precio);
 
-        res.status(200).json({ message: 'Agregado a pedidos con éxito', 'ID-CAMISAS-SERVICIOS': idServicioInsertado });
+        res.status(200).json({ 'ID-CAMISAS-SERVICIOS': idServicioInsertado, message: 'Agregado a pedidos con éxito' });
     } catch (error) {
         console.error('Error al agregar a pedidos:', error);
         res.status(500).json({ error: 'Error interno del servidor' });
     } finally {
         if (con) {
-            con.end(); // Asegúrate de cerrar la conexión en cualquier caso
+            con.end();
         }
     }
 });
 
 async function insertarServicio(con, res, idCamisa, tipo, tamano, calidad, area, precio, imagen) {
-    return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO SERVICIOS (\`TIPO-SERVICIO\`, \`tamaño\`, \`calidad\`, \`AREA\`, \`PRECIO\`, \`IMAGEN\`)
-        VALUES (?, ?, ?, ?, ?, ?)`;
-        con.query(sql, [tipo, tamano, calidad, area, precio, imagen], (err, results) => {
-            if (err) {
-                console.error('Error al insertar servicio:', err);
-                res.status(500).json({ error: 'Error interno del servidor al insertar servicio' });
-                reject(err);
-            } else {
-                const idServicioInsertado = results.insertId;
-                console.log('Inserción exitosa en SERVICIOS. ID del último insertado:', idServicioInsertado);
+    const sql = `INSERT INTO SERVICIOS (\`TIPO-SERVICIO\`, \`tamaño\`, \`calidad\`, \`AREA\`, \`PRECIO\`, \`IMAGEN\`)
+    VALUES (?, ?, ?, ?, ?, ?)`;
 
-                // Insertar a la tabla CAMISAS-SERVICIOS
-                insertarCamisasServicios(con, res, idCamisa, idServicioInsertado, precio);
+    try {
+        const results = await con.query(sql, [tipo, tamano, calidad, area, precio, imagen]);
+        const idServicioInsertado = results.insertId;
 
-                // Modificar la respuesta para incluir el ID del servicio
-                res.status(200).json({ 'ID-CAMISAS-SERVICIOS': idServicioInsertado });
-                resolve(idServicioInsertado);
-            }
-        });
-    });
+        console.log('Inserción exitosa en SERVICIOS. ID del último insertado:', idServicioInsertado);
+
+        // Insertar a la tabla CAMISAS-SERVICIOS
+        await insertarCamisasServicios(con, res, idCamisa, idServicioInsertado, precio);
+
+        // Modificar la respuesta para incluir el ID del servicio
+        return idServicioInsertado;
+    } catch (error) {
+        console.error('Error al insertar servicio:', error);
+        res.status(500).json({ error: 'Error interno del servidor al insertar servicio' });
+        throw error; // Para que el error se propague y sea manejado en el bloque catch del controlador principal
+    }
 }
 
-async function insertarCamisasServicios(con, idCamisa, idServicio, precio) {
-    return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO \`CAMISAS-SERVICIOS\` (\`ID-CAMISAS\`, \`ID-SERVICIOS\`, \`PRECIO\`) VALUES (?, ?, ?)`;
-        con.query(sql, [idCamisa, idServicio, precio], (err, result) => {
-            if (err) {
-                console.error('Error al insertar servicio en CAMISAS-SERVICIOS:', err);
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
-    });
+async function insertarCamisasServicios(con, res, idCamisa, idServicio, precio) {
+    const sql = `INSERT INTO \`CAMISAS-SERVICIOS\` (\`ID-CAMISAS\`, \`ID-SERVICIOS\`, \`PRECIO\`) VALUES (?, ?, ?)`;
+
+    try {
+        const result = await con.query(sql, [idCamisa, idServicio, precio]);
+        return result;
+    } catch (error) {
+        console.error('Error al insertar servicio en CAMISAS-SERVICIOS:', error);
+        res.status(500).json({ error: 'Error interno del servidor al insertar servicio en CAMISAS-SERVICIOS' });
+        throw error;
+    }
 }
 
 module.exports = router;
